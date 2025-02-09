8386ab9b5e47b51a26a8bf4cf89652c567012fb5
akashkumar62 <xiome555@gmail.com>
2025-02-09 18:04:06 +0530

    abc

--- server.js
+++ server.js
@@ -12,15 +12,16 @@ const dotenv = require("dotenv")
 dotenv.config();
 const app = express();
 
+
+app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5174",  // Replace with your frontend URL
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
   })
 );
-app.use(express.json());
-
 // Routes
 app.use("/auth", authRoutes);
 app.use("/participants", participantRoutes);
